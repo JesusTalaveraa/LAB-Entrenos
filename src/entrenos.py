bb86c9d1d7ad9617d5cee2392129dd4bdcb3a3e5
JesusTalaveraa <talaveracanoyisus@gmail.com>
2024-10-17 19:27:56 +0200

    Entrenos 2

--- src/entrenos.py
+++ src/entrenos.py
@@ -21,3 +21,15 @@ def lee_entrenos(ruta_csv):
             entrenos.append(tupla)
        return entrenos 
         
+def tipos_entrenos(entrenos):
+    tipos = set()
+    for e in entrenos:
+        tipos.add(e)
+    return sorted(list(tipos)) 
+
+def entrenos_duracion_superior(entrenos, d):
+    filtro = []
+    for e in entrenos:
+        if e.duracion >= d:
+            filtro.append(e)
+    return filtro
--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -5,7 +5,16 @@ def test_lee_entrenos():
     entrenos = lee_entrenos("data/entrenos.csv")
     print(entrenos[:3])
     print(entrenos[-3:])
-
+    
+def test_tipos_entrenos(entrenos):
+    print("Prueba de tipos de entreno")
+    print(tipos_entrenos)
+    
+def test_entrenos_duracion_superior(entrenos):
+    print("Prueba de 'entrenos_duracion_superior'")
+    print(entrenos_duracion_superior(entrenos, 75))
+    
 if __name__ == "__main__":
-    # entrenos = lee_entrenos()
-    test_lee_entrenos()
+    entrenos = lee_entrenos("data/entrenos.csv")
+    # test_lee_entrenos()
+    test_entrenos_duracion_superior(entrenos)
